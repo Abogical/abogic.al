dcb6c180b7b3f163218d3d8fe015584d71ed47d0
Abdelrahman Abdelrahman <git@abogic.al>
2025-02-08 15:03:07 +0100

    Revert "Update dependencies"
    This reverts commit d72fca4c6884f3b45705a996c1f8effb6943b7ce.

--- _config.ts
+++ _config.ts
@@ -14,12 +14,12 @@ import svgo from 'lume/plugins/svgo.ts';
 import terser from 'lume/plugins/terser.ts';
 import sourceMaps from 'lume/plugins/source_maps.ts';
 import postcss from 'lume/plugins/postcss.ts';
-import cssnano from 'npm:cssnano@^7.0.6';
-import postCssPresetEnv from 'npm:postcss-preset-env@^10.1.3';
-import puppeteer, { Browser } from 'npm:puppeteer@^24.2.0';
+import cssnano from 'npm:cssnano';
+import postCssPresetEnv from 'npm:postcss-preset-env';
+import puppeteer, { Browser } from 'npm:puppeteer';
 import minifyHTML from 'lume/plugins/minify_html.ts';
-import { join } from 'jsr:@std/path';
-import { formatISO } from 'npm:date-fns@4.1.0';
+import { join } from "https://deno.land/std@0.207.0/path/join.ts";
+import { formatISO } from 'npm:date-fns';
 
 const portStr = Deno.env.get('port');
 const port = portStr ? parseInt(portStr) : 3000;
@@ -58,92 +58,70 @@ site.use(postcss({
 site.use(sourceMaps());
 
 site.copy('external');
-site.copy(['.pdf']);
+site.copy(['.pdf'])
 
-site.filter('isoDate', (date) => formatISO(date, { representation: 'date' }));
+site.filter('isoDate', date => formatISO(date, {representation: 'date'}));
 
 // Metas plugin bug fix, where the links are incorrectly localhost.
 site.process(['.html'], (pages) => {
-	for (const page of pages) {
-		for (
-			const meta of page.document!.querySelectorAll(
-				'[property="og:image"], [property="og:url"]',
-			)
-		) {
-			meta.setAttribute(
-				'content',
-				meta.getAttribute('content')!.replace(
-					'http://localhost:3000',
-					'https://abogic.al',
-				),
-			);
-		}
-	}
-});
+    for(const page of pages){
+        for(const meta of page.document!.querySelectorAll('[property="og:image"], [property="og:url"]')){
+            meta.setAttribute('content', meta.getAttribute('content')!.replace('http://localhost:3000', 'https://abogic.al'))
+        }
+    }
+})
 
-if (Deno.env.get('BUILD_MODE')) {
-	site.ignore('cover-letter.md');
-}
+if(Deno.env.get('BUILD_MODE'))
+    site.ignore('cover-letter.md');
 
 const buildResumePDF = async (browser: Browser) => {
-	const page = await browser.newPage();
-	await page.goto('http://localhost:3000/resume/', {waitUntil: 'load', timeout: 0});
-	await Deno.writeFile(
-		'_site/resume.pdf',
-		new Uint8Array(await page.pdf({
-			format: 'A4',
-			pageRanges: '1',
-		})),
-	);
-	await page.close();
-};
+    const page = await browser.newPage();
+    await page.goto('http://127.0.0.1:3000/resume/');
+    await Deno.writeFile('_site/resume.pdf', await page.pdf({
+        format: 'A4',
+        pageRanges: '1',
+    }));
+    await page.close();
+}
 
-const screenshotPage = async (
-	browser: Browser,
-	url: string,
-	destination: string,
-) => {
-	const page = await browser.newPage();
-	await page.emulateMediaFeatures([
-		{ name: 'prefers-color-scheme', value: 'light' },
-	]);
-	await page.setViewport({
-		width: 600,
-		height: 300,
-	});
-	await page.goto(join('http://localhost:3000', url), {waitUntil: 'load', timeout: 0});
-	await page.screenshot({
-		path: join('_site', destination),
-	});
-	await page.close();
-};
+const screenshotPage = async (browser: Browser, url: string, destination: string) => {
+    const page = await browser.newPage();
+    await page.emulateMediaFeatures([
+        {name: 'prefers-color-scheme', value: 'light'},
+    ]);
+    await page.setViewport({
+        width: 600,
+        height: 300
+    });
+    await page.goto(join('http://127.0.0.1:3000', url))
+    await page.screenshot({
+        path: join('_site', destination)
+    })
+    await page.close();
+}
 
 const puppeteerTasks = async () => {
 	console.log('Running puppeteer tasks');
 	const browser = await puppeteer.launch();
 
-	const tasks = [];
-	tasks.push(buildResumePDF(browser));
-	const screenshots_urls = [
-		'/assets/index-og-image.css',
-	];
+    const tasks = []
+    tasks.push(buildResumePDF(browser));
+    const screenshots_urls = [
+        '/assets/index-og-image.css'
+    ];
 
-	for (const page of site.pages) {
-		if ('screenshot' in page.data) {
-			tasks.push(screenshotPage(browser, page.data.url, page.data.screenshot));
-			screenshots_urls.push(page.data.url);
-		}
-	}
-
-	await Promise.all(tasks);
+    for(const page of site.pages){
+        if('screenshot' in page.data){
+            tasks.push(screenshotPage(browser, page.data.url, page.data.screenshot));
+            screenshots_urls.push(page.data.url);
+        }
+    }
+	
+    await Promise.all(tasks);
 	await browser.close();
 	console.log('Puppeteer tasks done');
 	if (Deno.env.get('BUILD_MODE')) {
-		await Promise.all(
-			screenshots_urls.map((url) =>
-				Deno.remove(join('_site', url), { recursive: true })
-			),
-		);
+        await Promise.all(screenshots_urls.map(url => Deno.remove(join('_site', url), {recursive: true})));
 		Deno.exit();
 	}
 };
@@ -153,19 +131,18 @@ site.addEventListener('afterStartServer', puppeteerTasks);
 const puppeteerFiles = new Set([
 	'/resume/',
 	'/assets/resume.css',
-	'/assets/global.css',
+	'/assets/global.css'
 ]);
 site.addEventListener('afterUpdate', (event) => {
-	for (const page of event.pages) {
+	for (const page of event.pages)
 		if (puppeteerFiles.has(page.data.url) || 'screenshot' in page.data) {
 			puppeteerTasks();
-			break;
+            break;
 		}
-	}
 });
 
-Deno.addSignalListener('SIGINT', () => {
-	Deno.exit();
+Deno.addSignalListener("SIGINT", () => {
+    Deno.exit();
 });
 
 export default site;
